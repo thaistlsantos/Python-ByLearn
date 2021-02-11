8b5fc7e41c7b1a589eaf9c805b4985aea8896809
Thais de Lima Santos <thais_tlsan@hotmail.com>
2021-02-11 00:27:55 -0300

    Create 001_estruturadeumprograma.py

---
+++ 001_estruturadeumprograma.py
@@ -0,0 +1,10 @@
+# O básico da programação
+# def -> Define uma função 
+
+def main():
+    #Comandos
+    pass
+
+#------------------------
+#chamada da funcao principal, irá executar o programa
+    main()
