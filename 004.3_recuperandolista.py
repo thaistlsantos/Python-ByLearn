b74eb163627b6c3f51f2494248678e001681b777
Thaís de Lima Santos <67654045+thaistlsantos@users.noreply.github.com>
2022-04-21 00:08:03 -0300

    Ex

---
+++ 004.3_recuperandolista.py
@@ -0,0 +1,9 @@
+# Para pegar um valor da lista, usamos o índice do elemento que queremos
+
+lista_a = [1, 2, 3]
+elemento1 = lista_a[0]
+elemento2 = lista_a[1]
+elemento3 = lista_a[2]
+print(elemento1)
+print(elemento2)
+print(elemento3)
