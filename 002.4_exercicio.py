915534d68abe966d0a60c2c4244096f55042822e
Thaís de Lima Santos <67654045+thaistlsantos@users.noreply.github.com>
2022-04-20 21:40:27 -0300

    atz

--- 002.4_exercicio.py
+++ 002.4_exercicio.py
@@ -5,6 +5,10 @@ def main():
     num1 = float(input("Digite o primeiro número: "))
     num2 = float(input("Digite o segundo número: "))
     resp = num1 / num2
+
+    print("O tipo de A é: ", type(num1))
+    print("O tipo de B é: ", type(num2))
+
     print('O valor da divisão entre {} e {} é: {}'.format(num1, num2, resp))
 
 
--- 002.5_exercicio.py
+++ 002.5_exercicio.py
@@ -1,4 +1,4 @@
-# Desafio Exercício 2.4
+# Desafio Exercício 2.5
 # Fazer uma função que subtraia quatro números entrados pelo usuário
 
 def main():
@@ -7,5 +7,8 @@ def main():
     num3 = float(input("Digite o primeiro número: "))
     num4 = float(input("Digite o segundo número: "))
     resp = num1 - num2 - num3 - num4
-    print(f'O valor da subtração entre {num1}, {num2}, {num3} e {num4} é: ', resp)
+    print(
+        f'O valor da subtração entre {num1}, {num2}, {num3} e {num4} é: ', resp)
+
+
 main()
