665ae86fd515d438a3b7b6011d9c098b26189cd7
Thaís de Lima Santos <67654045+thaistlsantos@users.noreply.github.com>
2022-04-20 22:21:17 -0300

    Ex

--- 003_listas.py
+++ 003_listas.py
@@ -1,9 +1,17 @@
 # Motivação das listas: Trabalhar com vários valores relacionados em uma só variável
 
+# Todos os possiveis alunos
+# Felipe, Alisson, Haynes, ByLearner
+# As notas respectivas dos alunos
+# 8 e 10, 10 e 7, 8 e 9, 10 e 10
+
+# Media de [Primeiro Aluno] = [Notas do 1º] / 2
+# Para todos -> Media do [Atual] = [Notas do Atual] /2
+
 def main():
     primeira_lista = ['Felipe', 'Alisson', 'Haynes', 'ByLearner']
-    #------Posição------ 1º ------ 2º ------- 3º ------ 4º ------
-    #------Índice--------0---------1----------2---------3--------
+    # ------Posição------ 1º ------ 2º ------- 3º ------ 4º ------
+    # ------Índice--------0---------1----------2---------3--------
 
     print(primeira_lista[0])
     print(primeira_lista[1])
@@ -13,11 +21,15 @@ def main():
     primeira_lista.append('Thais')
 
     print(primeira_lista)
+    qtd = len(primeira_lista)
+    print("Sua lista possui : ", qtd, "elementos")
+
+
 main()
 
 # Métodos alternativos de criar listas
 
-lista = [1,2,3,4]
-lista2 = ['a','b','c']
-lista3 = ['a','b',2,3]
+lista = [1, 2, 3, 4]
+lista2 = ['a', 'b', 'c']
+lista3 = ['a', 'b', 2, 3]
 lista_alunos = []
